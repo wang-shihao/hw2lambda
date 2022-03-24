75c249fed11f85f7f95c1beb6bdc18ff97714fcf
wang-shihao <sw3567@columbia.edu>
2022-03-24 01:10:40 -0400

    init commit

---
+++ indexphotos.py
@@ -0,0 +1,62 @@
+import boto3
+import json
+import datetime
+import requests
+
+
+s3 = boto3.client('s3')
+rek_client = boto3.client('rekognition', region_name='us-east-1')
+
+
+def lambda_handler(event, context):
+    # test demo
+    print("event")
+    print(event)
+    s3_info = event['Records'][0]['s3']
+    print("Info")
+    print(s3_info)
+    bucket_name = s3_info['bucket']['name']
+    photo_name = s3_info['object']['key']
+    print(photo_name, bucket_name)
+    # get the labels of photo
+    labels = detectLabels(bucket_name, photo_name)
+    print("---------labels from Reko -------", labels)
+
+    # get metadata from s3
+    headObject = s3.head_object(Bucket=bucket_name, Key=photo_name)
+    metaData = headObject['Metadata']
+    modify_time = headObject['LastModified']
+
+    for i, label in metaData.items():
+        labels.append(str(label).lower())
+
+    # create JSON array with labels
+    json_object = {
+        "objectKey": photo_name,
+        "bucket": bucket_name,
+        "createdTimestamp": modify_time.strftime("%y-%m-%d %H:%M:%S"),
+        "labels": labels
+    }
+    print("-------------payload----------- ", json_object)
+
+    # upload data to OpenSearch
+    endpoint = 'https://search-photo-dcoxllgzvsuh2jzv6cj5soxojq.us-east-1.es.amazonaws.com/photo/_doc'
+    headers = {'Content-Type': 'application/json'}
+    auth = ('coms6998', 'Coms6998!')
+    response = requests.post(endpoint, json=json_object,
+                             auth=auth, headers=headers)
+    print(response)
+    return {'response': str(response)}
+
+
+def detectLabels(bucket, photo):
+    print("Inside")
+    print(photo, bucket)
+    response = rek_client.detect_labels(
+        Image={'S3Object': {'Bucket': bucket, 'Name': photo}}, MinConfidence=75)
+    labels = []
+    for label in response['Labels']:
+        labels.append(str(label['Name']).lower())
+        if len(labels) > 4:
+            break
+    return labels
---
+++ searchphotos.py
@@ -0,0 +1,71 @@
+import json
+import boto3
+import requests
+import os
+import logging
+
+import inflect
+
+
+def lambda_handler(event, context):
+
+    print(event)
+    # query_text = "show me tree"
+    query_text = event["queryStringParameters"]['q']
+    labels = getPhotoLabel(query_text)
+
+    print(query_text)
+
+    base_url = 'https://search-photo-dcoxllgzvsuh2jzv6cj5soxojq.us-east-1.es.amazonaws.com/'
+    auth = ('coms6998', 'Coms6998!')
+    headers = {'Content-Type': 'application/json'}
+
+    paths_set = set()
+    for label in labels:
+        url = base_url + '/_search?q=labels:' + label
+        response = requests.get(url, auth=auth, headers=headers)
+        response = response.json()
+
+        for info in response['hits']['hits']:
+            photo_data = info['_source']
+            photo_bucket = photo_data['bucket']
+            photo_key = photo_data['objectKey']
+
+            # check url format
+            photo_url = 'https://{}.s3.amazonaws.com/{}'.format(
+                photo_bucket, photo_key)
+            paths_set.add(photo_url)
+
+    photo_paths = list(paths_set)
+    print(photo_paths)
+
+    lam_response = {
+        "statusCode": 200,
+        "headers": {"Access-Control-Allow-Origin": "*", "Content-Type": "application/json"},
+        "body": json.dumps(photo_paths),
+        "isBase64Encoded": False
+    }
+    return lam_response
+
+
+def getPhotoLabel(query_text):
+    client = boto3.client('lex-runtime')
+    lex_response = client.post_text(
+        botName='photo_query',
+        botAlias='$LATEST',
+        userId='coms6998',
+        inputText=query_text)
+    labels = []
+    if 'slots' in lex_response:
+        slots = lex_response['slots']
+        for key, value in slots.items():
+            if value != None:
+                p = inflect.engine()
+                if p.singular_noun(value):
+                    singular_word = p.singular_noun(value)
+                else:
+                    singular_word = value
+
+                # singular_value =  value.rstrip('s')
+                labels.append(singular_word)
+        return labels
