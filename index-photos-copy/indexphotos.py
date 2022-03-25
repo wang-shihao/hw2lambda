fb4d3b3b7565fa744f66bf00bd2405800c3c2a35
yuxi18 <96137555+yuxi18@users.noreply.github.com>
2022-03-24 23:05:58 -0400

    Add files via upload

--- index-photos-copy/indexphotos.py
+++ index-photos-copy/indexphotos.py
@@ -8,6 +8,7 @@ s3 = boto3.client('s3')
 rek_client = boto3.client('rekognition', region_name='us-east-1')
 
 
+
 def lambda_handler(event, context):
     # test demo
     print("event")
@@ -21,12 +22,12 @@ def lambda_handler(event, context):
     # get the labels of photo
     labels = detectLabels(bucket_name, photo_name)
     print("---------labels from Reko -------", labels)
-
+    
     # get metadata from s3
     headObject = s3.head_object(Bucket=bucket_name, Key=photo_name)
     metaData = headObject['Metadata']
     modify_time = headObject['LastModified']
-
+    
     for i, label in metaData.items():
         labels.append(str(label).lower())
 
@@ -42,21 +43,26 @@ def lambda_handler(event, context):
     # upload data to OpenSearch
     endpoint = 'https://search-photo-dcoxllgzvsuh2jzv6cj5soxojq.us-east-1.es.amazonaws.com/photo/_doc'
     headers = {'Content-Type': 'application/json'}
-    auth = ('coms6998', 'Coms6998!')
-    response = requests.post(endpoint, json=json_object,
-                             auth=auth, headers=headers)
+    auth = ('coms6998','Coms6998!')
+    response = requests.post(endpoint, json = json_object, auth = auth, headers = headers)
     print(response)
     return {'response': str(response)}
+
+
 
 
 def detectLabels(bucket, photo):
     print("Inside")
     print(photo, bucket)
-    response = rek_client.detect_labels(
-        Image={'S3Object': {'Bucket': bucket, 'Name': photo}}, MinConfidence=75)
+    response = rek_client.detect_labels(Image={'S3Object': {'Bucket': bucket, 'Name': photo}}, MinConfidence=75)
     labels = []
     for label in response['Labels']:
         labels.append(str(label['Name']).lower())
         if len(labels) > 4:
             break
     return labels
+
+
+
+
+
